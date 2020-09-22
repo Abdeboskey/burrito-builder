a57f4bbf4b16ebdda870a05696cfa04370e74c2f
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-22 11:28:59 -0600

    Create tests for loading and posting

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,78 @@
+import React from 'react'
+import App from './App'
+import { fireEvent, getByText, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom'
+import { placeOrder, getOrders } from '../../apiCalls'
+jest.mock('../../apiCalls')
 
+describe('App', () => {
+
+  let existingOrders, newOrder
+
+  beforeEach(() => {
+    existingOrders = {
+      orders: [
+        {
+          id: 1,
+          name: 'Bubs',
+          ingredients: ['beans', 'queso fresco']
+        },
+        {
+          id: 2,
+          name: 'Jimmy',
+          ingredients: ['carnitas', 'hot sauce', 'cilantro']
+        },
+        {
+          id: 3,
+          name: 'Xadie',
+          ingredients: ['sofritas', 'lettuce', 'guacamole']
+        }
+      ]
+    }
+    newOrder = {
+      id: 24,
+      name: 'Bat Man',
+      ingredients: ['carnitas', 'guacamole']
+    }
+  })
+
+  it('should display saved orders when component is loaded', async () => {
+    getOrders.mockResolvedValueOnce(existingOrders)
+    const { findByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+
+    const bubsOrder = await findByRole('heading', { name: /bubs/i })
+    const jimmyOrder = await findByRole('heading', { name: /jimmy/i })
+    const xadieOrder = await findByRole('heading', { name: /xadie/i })
+  })
+
+  it('should display a new order when one has been successfully placed', async () => {
+    getOrders.mockResolvedValueOnce(existingOrders)
+    placeOrder.mockResolvedValueOnce(newOrder)
+    const { findByRole, getByPlaceholderText, getByRole  } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+
+    const nameInput = getByPlaceholderText(/name/i);
+    fireEvent.change(nameInput, { target: { value: "Bat Man" } });
+
+    const carnitasButton = getByRole("button", { name: /carnitas/i });
+    fireEvent.click(carnitasButton);
+
+    const guacamoleButton = getByRole("button", { name: /guacamole/i });
+    fireEvent.click(guacamoleButton);
+
+    const submitButton = getByRole("button", { name: /submit order/i });
+    fireEvent.click(submitButton)
+
+    const successfulOrder = await findByRole('heading', {name: /bat man/i})
+
+    expect(successfulOrder).toBeInTheDocument()
+  })
+})
