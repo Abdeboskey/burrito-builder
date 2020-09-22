85577b4766bf805bdf71488b8a2b70f00da2eb0d
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-22 10:24:33 -0600

    Create post new order method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,28 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw response;
+        }
+      })
+}
+
+export const placeOrder = state => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: state.name,
+      ingredients: state.ingredients
+    })
+  })
+  .then(response => {
+		if (response.ok) {
+			return response.json()
+		} else {
+			throw response
+		}
+	})
 }
